e7b5097308e77ad30bfd22d3d70a2ba072667a16
LOLOXI7 <ocipereirajunior@gmail.com>
2025-02-26 20:17:21 -0300

    Tarefa finalizada

---
+++ FixacaoConceitosBasicos1/Model/CalculaAumento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixacaoConceitosBasicos1.Model
+{
+    internal class CalculaAumento
+    {
+        package Model;
+
+        public class CalculaAumento
+        {
+            public static double Calcular(double salario, double aumento)
+            {
+                return salario + aumento;
+            }
+        }
+    }
+}
